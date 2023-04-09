2729a8385db29ff33a5daea422678281676dc06f
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-04-09 12:58:11 -0700

    Update moongose connect info

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 HelloController(app);
 UserController(app);
