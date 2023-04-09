1defc62b78067266950352f2b92787b4be8787fa
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-04-09 16:00:07 -0700

    Update variables to log to mongoDB Atlas

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 
-//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+mongoose.connect(CONNECTION_STRING);
 
 HelloController(app);
 UserController(app);
 TuitsController(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
